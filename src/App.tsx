3c8328260e65fe1017133b281f3ff6ea544e8550
pmagalhaes2 <patricia.magalhaes2@gmail.com>
2022-10-13 23:07:24 -0300

    chore: use tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./styles/global.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-xl text-violet-500">Hello, world!</h1>
+      <button className="bg-cyan-500 font-md px-4 py-2 rounded text-white hover:bg-cyan-300">Enviar</button>
+    </>
+  );
+}
+
+export default App;
